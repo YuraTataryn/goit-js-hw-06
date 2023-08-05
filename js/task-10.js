84c23eb96406c6a930be063de22a98499579d492
YuraTataryn <127976814+YuraTataryn@users.noreply.github.com>
2023-08-05 07:36:58 -0700

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const controlsDiv = document.querySelector('#controls');
+  const input = controlsDiv.querySelector('input');
+  const createButton = controlsDiv.querySelector('[data-create]');
+  const destroyButton = controlsDiv.querySelector('[data-destroy]');
+  const boxesDiv = document.querySelector('#boxes');
+
+  createButton.addEventListener('click', function () {
+    const amount = parseInt(input.value);
+
+    if (isNaN(amount)) {
+      return;
+    }
+
+    const boxes = [];
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxes.push(box);
+    }
+
+    boxesDiv.append(...boxes);
+  });
+
+  destroyButton.addEventListener('click', function () {
+    boxesDiv.innerHTML = '';
+  });
+});
+
+
+
+
+
+
+
