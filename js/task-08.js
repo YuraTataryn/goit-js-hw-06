1b9a6ad082b9b59c7f15b19991363f6461fca721
YuraTataryn <127976814+YuraTataryn@users.noreply.github.com>
2023-08-05 07:37:02 -0700

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    if (emailInput.value === '' || passwordInput.value === '') {
+      alert('All fields must be filled out.');
+    } else {
+      const formData = {
+        email: emailInput.value,
+        password: passwordInput.value
+      };
+
+      console.log(formData);
+      loginForm.reset();
+    }
+  });
+});
